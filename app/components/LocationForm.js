73dcdbbad78722cb47526057dfdf8905895e5ad4
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2018-03-19 15:02:55 +0000

    created string functions to split out city and country

--- app/components/LocationForm.js
+++ app/components/LocationForm.js
@@ -1,16 +1,76 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+var Link = require('react-router-dom').Link;
+
+var isLocationValid = require('../utils/string').isLocationValid;
+
 class LocationInput extends React.Component {
   constructor (props) {
     super(props);
 
     this.state = {
+      location: props.val,
       city: '',
-      country: ''
+      country: '',
+      valid: props.valid,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  // check if value is valid,
+  // if no disable button
+  handleChange(event) {
+    var val = event.target.value;
+    var isValid = isLocationValid(val);
+    this.setState(function() {
+      return {
+        location: val,
+        valid: isValid
+      };
+    });
+  }
+
+  handleSubmit(event) {
+    
+  }
+
+  render () {
+    return (
+      <div className={this.props.style}>
+        <input
+          id='location'
+          placeholder={this.state.location}
+          type='text'
+          autoComplete='off'
+          value={this.state.location}
+          onChange={this.handleChange}
+        />
+        <Link
+          className='submit'
+          type='button'
+          onClick={this.handleSubmit}
+          disabled={!this.state.valid}
+        >
+          Submit
+        </Link>
+      </div>
+    );
+  }
 }
+
+LocationInput.propTypes = {
+  location: PropTypes.string.isRequired,
+  style: PropTypes.string.isRequired,
+  valid: PropTypes.bool.isRequired,
+};
+
+location.defaultProps = {
+  location: 'London, UK',
+  style: 'vertical',
+  valid: true
+};
+
+module.exports = LocationInput;
